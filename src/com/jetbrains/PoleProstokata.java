b0ced6f50a0887afc686fd909526bb0865d7b3a9
vanndhall <szymon.mantey@wp.pl>
2019-02-26 22:24:00 +0100

    zadanie 1.1

---
+++ src/com/jetbrains/PoleProstokata.java
@@ -0,0 +1,31 @@
+package com.jetbrains;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PoleProstokata {
+
+    public static void main(String[] args)
+    throws IOException
+    {
+        double a,b;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Porgram oblicza pole prostokąta.");
+        System.out.println("Podaj bok  A: ");
+        a = Double.parseDouble(br.readLine());
+        System.out.println("Podaj bok B: ");
+        b = Double.parseDouble(br.readLine());
+
+        System.out.print("Pole prostokąta o boku a= "+ a+ " i boku b= "+b);
+        System.out.println(" wynosi "+ Oblicz(a,b));
+
+
+    }
+
+    public static Double Oblicz(double a, double b){
+        double oblicz =a*b;
+        return oblicz;
+    }
+
+}
